824641f041b4e49498f33b242c717fde213c98f9
chrisc2389 <92484730+chrisc2389@users.noreply.github.com>
2024-03-19 21:35:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from datetime import date
 import holidays
 import json
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Body(BaseModel):
+    state: str
+    year: str
+
 @app.get("/get-state-holidays")
-def get_state_holidays():
+async def get_state_holidays(body: Body):
     data = {}
-    us_holidays = holidays.UnitedStates(state='TX',years=2023)
+    #us_holidays = holidays.UnitedStates(state='TX',years=2023)
+    us_holidays = holidays.UnitedStates(state=body.state,years=body.year)
 
     # Print all holidays in the year 2024
     for date, name in sorted(us_holidays.items()):
